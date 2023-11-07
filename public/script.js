e3ea863587aea3875407a522cbc58c104acdd173
nandhakrishnaboddu <nandhakrishnasai2003@gmail.com>
2023-11-07 16:20:07 +0530

    changed database from sql to mongodb and done few changes

--- public/script.js
+++ public/script.js
@@ -126,33 +126,185 @@ function selecteddate(id) {
         headers: {
           'Content-Type': 'application/json',
         },
-      }).then(response => {
-        console.log(response.data);
+      }).then(response=>{
         result=response.data;
+        console.log(result);
         document.getElementById("viewcontent").style.display="none";
+        let s=["BreakFast","Lunch","Dinner","FullDay"];
+        for(let session_value of s){
+            let outerdiv=document.createElement("div");
+            let innerdiv=document.createElement("div");
+            let h2ele=document.createElement("p");
+            h2ele.classList.add("sessionheading");
+            let Donor=document.createElement("p");
+            Donor.id="don"+session_value;
+            let phone=document.createElement("p");
+            phone.id="pon"+session_value;
+            h2ele.textContent=session_value;
+            Donor.textContent="Donor Name : ----";
+            phone.textContent="Devotee Name: --- ";
+            let viewbutton=document.createElement("button");
+            viewbutton.textContent="Book";
+            viewbutton.id="btn"+session_value;
+            viewbutton.classList.add("viewbutton");
+            viewbutton.onclick = function(){
+                let inputdate=document.getElementById("date");
+                inputdate.readOnly=false;
+                inputdate.value=id;
+                inputdate.readOnly=true;
+                let ele=session_value;
+                    if(ele==="FullDay"){
+                        let sessionele=document.getElementById("BreakFast");
+                        sessionele.disabled=true;
+                        let labelele=sessionele.labels[0];
+                        console.log(labelele);
+                        labelele.style.textDecoration = "line-through";
+                        sessionele=document.getElementById("Lunch");
+                        sessionele.disabled=true;
+                        labelele=sessionele.labels[0];
+                        console.log(labelele);
+                        labelele.style.textDecoration = "line-through";
+                        sessionele=document.getElementById("Dinner");
+                        sessionele.disabled=true;
+                        labelele=sessionele.labels[0];
+                        console.log(labelele);
+                        // labelele.style.textDecoration = "line-through";
+                        // sessionele=document.getElementById("FullDay");
+                        // sessionele.disabled=true;
+                        // labelele=sessionele.labels[0];
+                        // console.log(labelele);
+                        labelele.style.textDecoration = "line-through";
+                    }
+                    else if(ele==="BreakFast"){
+                        // let sessionele=document.getElementById("BreakFast");
+                        // sessionele.disabled=true;
+                        // let labelele=sessionele.labels[0];
+                        // console.log(labelele);
+                        // labelele.style.textDecoration = "line-through";
+                        sessionele=document.getElementById("Lunch");
+                        sessionele.disabled=true;
+                        labelele=sessionele.labels[0];
+                        console.log(labelele);
+                        labelele.style.textDecoration = "line-through";
+                        sessionele=document.getElementById("Dinner");
+                        sessionele.disabled=true;
+                        labelele=sessionele.labels[0];
+                        console.log(labelele);
+                        labelele.style.textDecoration = "line-through";
+                        sessionele=document.getElementById("FullDay");
+                        sessionele.disabled=true;
+                        labelele=sessionele.labels[0];
+                        console.log(labelele);
+                        labelele.style.textDecoration = "line-through";
+                    }
+                    else if(ele==="Lunch"){
+                        let sessionele=document.getElementById("BreakFast");
+                        sessionele.disabled=true;
+                        let labelele=sessionele.labels[0];
+                        console.log(labelele);
+                        // labelele.style.textDecoration = "line-through";
+                        // sessionele=document.getElementById("Lunch");
+                        // sessionele.disabled=true;
+                        // labelele=sessionele.labels[0];
+                        // console.log(labelele);
+                        labelele.style.textDecoration = "line-through";
+                        sessionele=document.getElementById("Dinner");
+                        sessionele.disabled=true;
+                        labelele=sessionele.labels[0];
+                        console.log(labelele);
+                        labelele.style.textDecoration = "line-through";
+                        sessionele=document.getElementById("FullDay");
+                        sessionele.disabled=true;
+                        labelele=sessionele.labels[0];
+                        console.log(labelele);
+                        labelele.style.textDecoration = "line-through";
+                    }
+                    else{
+                        let sessionele=document.getElementById("BreakFast");
+                        sessionele.disabled=true;
+                        let labelele=sessionele.labels[0];
+                        console.log(labelele);
+                        labelele.style.textDecoration = "line-through";
+                        sessionele=document.getElementById("Lunch");
+                        sessionele.disabled=true;
+                        labelele=sessionele.labels[0];
+                        console.log(labelele);
+                        // labelele.style.textDecoration = "line-through";
+                        // sessionele=document.getElementById("Dinner");
+                        // sessionele.disabled=true;
+                        // labelele=sessionele.labels[0];
+                        // console.log(labelele);
+                        labelele.style.textDecoration = "line-through";
+                        sessionele=document.getElementById("FullDay");
+                        sessionele.disabled=true;
+                        labelele=sessionele.labels[0];
+                        console.log(labelele);
+                        labelele.style.textDecoration = "line-through";
+                    }
+                form.classList.add("popup");
+                contentdiv.style.display="none";
+            };   
+            outerdiv.appendChild(h2ele);
+            innerdiv.appendChild(Donor);
+            innerdiv.appendChild(phone);
+            outerdiv.id="id"+session_value;
+            outerdiv.appendChild(innerdiv);
+            outerdiv.appendChild(viewbutton);
+            outerdiv.classList.add("outerdiv");
+            innerdiv.classList.add("innerdiv");
+            contentdiv.appendChild(outerdiv); 
+        }
         if(result.length==0){
-            console.log("hello");
-            let divele=document.createElement("div");
-            let passageele=document.createElement("p");
-            passageele.innerText="No slots booked";
-            passageele.classList.add("sessionheading");
-            divele.appendChild(passageele);
-            contentdiv.appendChild(divele);
+            console.log("resukt");
         }
         else{
+            console.log(result);
             result.forEach(ele=>{
-                let outerdiv=document.createElement("div");
-                let innerdiv=document.createElement("div");
-                let h2ele=document.createElement("p");
-                h2ele.classList.add("sessionheading");
-                let donar=document.createElement("p");
-                let phone=document.createElement("p");
-                h2ele.textContent=ele.Session;
-                donar.textContent="Donar Name : "+ele.Donar_Name
-                phone.textContent="Phone : "+ele.Phone;
-                let viewbutton=document.createElement("button");
-                viewbutton.textContent="View";
-                viewbutton.classList.add("viewbutton");
+                let id_value=ele.Session;
+                if(id_value=="FullDay"){
+                    let sessionele=document.getElementById("BreakFast");
+                    sessionele.disabled=true;
+                    let labelele=sessionele.labels[0];
+                    console.log(labelele);
+                    labelele.style.textDecoration = "line-through";
+                    sessionele=document.getElementById("Lunch");
+                    sessionele.disabled=true;
+                    labelele=sessionele.labels[0];
+                    console.log(labelele);
+                    labelele.style.textDecoration = "line-through";
+                    sessionele=document.getElementById("Dinner");
+                    sessionele.disabled=true;
+                    labelele=sessionele.labels[0];
+                    console.log(labelele);
+                    s.forEach(val=>{
+                        if(val!="FullDay"){
+                        let sbtn=document.getElementById("btn"+val);
+                        sbtn.textContent="view";
+                        sbtn.onclick=()=>{
+                            alert("full day wad already booked");
+                        }
+                        }
+                     })
+                    
+                }
+                else{
+                    s.forEach(val=>{
+                        if(val=="FullDay"){
+                        let sbtn=document.getElementById("btn"+val);
+                        sbtn.textContent="view";
+                        sbtn.onclick=()=>{
+                            alert(id_value+" is already booked");
+                        }
+                        }
+                     })
+                }
+                let donele=document.getElementById("don"+id_value);
+                let ponele=document.getElementById("pon"+id_value);
+                donele.textContent="Donor Name:"+ele.Donor_Name;
+                ponele.textContent="Devotee Name:"+ele.Booked_By;
+               
+                let viewbutton=document.getElementById("btn"+id_value);
+                 viewbutton.textContent="View";
 
                 viewbutton.onclick=()=>{
                 contentdiv.style.display="none";
@@ -168,36 +320,36 @@ function selecteddate(id) {
                 let viewsessionele=document.createElement("p");
                 viewsessionele.classList.add("sessionheading");
                 viewsessionele.style.fontSize="25px";
-                let viewdonarele=document.createElement("p");
+                let viewDonorele=document.createElement("p");
                 let phonenumber=document.createElement("p");
                 let willattend=document.createElement("p");
                 let bookedby=document.createElement("p");
                 let sevak_name=document.createElement("p");
-                let Donar_Id=document.createElement("p");
+                let Donor_Id=document.createElement("p");
                 let Ocassion=document.createElement("p");
                 let Reciept_Number=document.createElement("p"); 
                   
                 viewsessionele.textContent=ele.Session;
                 viewsessionele.style.fontWeight= "bold";
                 viewsessionele.style.textAlign="center";
-                viewdonarele.textContent="Donar Name  : "+ele.Donar_Name;
+                viewDonorele.textContent="Donor Name  : "+ele.Donor_Name;
                 phonenumber.textContent="Phone  : "+ele.Phone;
                 bookedby.textContent="Booked By  : "+ele.Booked_By;
                 willattend.textContent="willattend  : "+ele.will_attend;
                 sevak_name.textContent="Sevak_Name : "+ele.Sevak_Name;
-                Donar_Id.textContent="Donar ID :  "+ele.Donar_id;
+                Donor_Id.textContent="Donor ID :  "+ele.Donor_id;
                 Ocassion.textContent="Ocassion :  "+ele.Occassion;
                 Reciept_Number.textContent="Reciept Number : "+ele.Reciept_Number;   
                 
 
                 leftdiv.appendChild(viewsessionele);
-                leftdiv.appendChild(viewdonarele);
+                leftdiv.appendChild(viewDonorele);
                 leftdiv.appendChild(phonenumber);
                 leftdiv.appendChild(bookedby);
                 leftdiv.appendChild(willattend);
 
                 rightdiv.appendChild(sevak_name);
-                rightdiv.appendChild(Donar_Id);
+                rightdiv.appendChild(Donor_Id);
                 rightdiv.appendChild(Ocassion);
                 rightdiv.appendChild(Reciept_Number);
                 leftdiv.classList.add("leftdiv");
@@ -215,71 +367,16 @@ function selecteddate(id) {
                viewcontent.appendChild(viewclosebutton);
                viewcontent.style.display="block";
                  }
-                outerdiv.appendChild(h2ele);
-                innerdiv.appendChild(donar);
-                innerdiv.appendChild(phone);
-                outerdiv.appendChild(innerdiv);
-                outerdiv.appendChild(viewbutton);
-                outerdiv.classList.add("outerdiv");
-                innerdiv.classList.add("innerdiv");
-                contentdiv.appendChild(outerdiv); 
-            })
+             })
         }
-        if(flag){
-        let buttondivele=document.createElement("div");
-        buttondivele.classList.add("buttonele");
-        let btnele=document.createElement("button");
-        btnele.textContent="+";
-        buttondivele.appendChild(btnele);
-        console.log('Response from server:',result);
-        btnele.onclick = function(){
-            // let Session=document.getElementsByTagName("input");
-            // Session.forEach(ele=>{
-            //     ele.required=true;
-            // })
-            let inputdate=document.getElementById("date");
-            inputdate.readOnly=false;
-            inputdate.value=id;
-            inputdate.readOnly=true;
-            console.log(result);
-            result.forEach(ele=>{
-                if(ele.Session==="FullDay"){
-                    let sessionele=document.getElementById("BreakFast");
-                    sessionele.disabled=true;
-                    let labelele=sessionele.labels[0];
-                    console.log(labelele);
-                    labelele.style.textDecoration = "line-through";
-                    sessionele=document.getElementById("Lunch");
-                    sessionele.disabled=true;
-                    labelele=sessionele.labels[0];
-                    console.log(labelele);
-                    labelele.style.textDecoration = "line-through";
-                    sessionele=document.getElementById("Dinner");
-                    sessionele.disabled=true;
-                    labelele=sessionele.labels[0];
-                    console.log(labelele);
-                    labelele.style.textDecoration = "line-through";
-                    sessionele=document.getElementById("FullDay");
-                    sessionele.disabled=true;
-                    labelele=sessionele.labels[0];
-                    console.log(labelele);
-                    labelele.style.textDecoration = "line-through";
-                }
-                else{
-                   let sessionele=document.getElementById(ele.Session);
-                   sessionele.disabled=true;
-                   let labelele=sessionele.labels[0];
-                   labelele.style.textDecoration = "line-through";
-                   sessionele=document.getElementById("FullDay");
-                   sessionele.disabled=true;
-                    labelele=sessionele.labels[0];
-                   labelele.style.textDecoration = "line-through";
-                }
-            })
-            form.classList.add("popup");
-            contentdiv.style.display="none";
-        };
-        contentdiv.appendChild(buttondivele);
+        if(!flag){
+         s.forEach(val=>{
+            let sbtn=document.getElementById("btn"+val);
+            sbtn.textContent="view";
+            sbtn.onclick=()=>{
+                alert("Booking cannot be done in completed days");
+            }
+         })
         }
         contentdiv.style.display="block";
       })
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const mongoose = require('mongoose');
 // const mysql=require("mysql2");
 require('dotenv').config();
-const mysql = require("mysql2/promise");
+// const mysql = require("mysql2/promise");
 
 const port=process.env.PORT||3000;
 
 const cors = require('cors');
+const { ListSearchIndexesCursor } = require('mongodb');
 app.use(cors());
 
 app.use(express.static('public'));
@@ -15,18 +17,149 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+const url="mongodb+srv://harekrishna:harekrishna@cluster0.a3tfpck.mongodb.net/?retryWrites=true&w=majority"
+
+async function connect(){
+    try{
+        await mongoose.connect(url);
+        console.log("connected to database");
+    }catch(e){
+        console.error(e);
+    }
+} 
+connect();
+
+
+const Schema=mongoose.Schema;
+const sessionschema=new Schema({
+    Date:String,
+    Session:String,
+    Donor_Name:String,
+    Phone:String,
+    Booked_By:String,
+    Sevak_Name:String,
+    Donor_id:String,
+    Occassion:String,
+    Reciept_Number:String,
+    will_attend:String
+})
+
+const MyModel = mongoose.model('harekrishnaentry',sessionschema);
+
+async function search(date){
+    try{
+     return await MyModel.find({Date:date}).exec();
+    }catch(e){
+        console.error(e);
+    }  
+}
+
+app.post('/events',express.json(),(request, res) => {
+    const {date}=request.body;
+    console.log(date);
+    search(date).then(result=>{
+        res.send(result);
+    },(error)=>{
+        console.log(error);
+    });
+});
+
+
+app.post('/',(req, res) => {
+    let data = req.body;
+    console.log(data);
+    const newentry=new MyModel(data);
+    newentry.save();
+    res.redirect("/");
+});
+
+
+
 // Serve HTML file
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-console.log(process.env.USERNAM);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(process.env.USERNAM);
 // var connection=mysql.createConnection({
 //     host:process.env.HOSTNAME,
 //     user:process.env.USERNAM,
 //     password:process.env.PASSWORD,
 //     database:process.env.DATABASE,
 //     port:process.env.DATABASEPORT,
+//     connectionLimit:10,
 // })
 
 // connection.connect(function(err) {
@@ -34,60 +167,50 @@ console.log(process.env.USERNAM);
 //     console.log("Connected!");
 // });
 
-const connection = mysql.createPool({
-  host:process.env.HOSTNAME,
-  user:process.env.USERNAM,
-  password:process.env.PASSWORD,
-  database:process.env.DATABASE,
-  port:process.env.DATABASEPORT,
-  // waitForConnections: true,
-  // connectionLimit: 10,
-  // maxIdle: 10,
-  // idleTimeout: 60000,
-  // queueLimit: 0
-});
+// // const connection = mysql.createPool({
+// //   host:process.env.HOSTNAME,
+// //   user:process.env.USERNAM,
+// //   password:process.env.PASSWORD,
+// //   database:process.env.DATABASE,
+// //   port:process.env.DATABASEPORT,
+// //   waitForConnections: true,
+// //   connectionLimit: 10,
+// //   maxIdle: 10,
+// //   idleTimeout: 60000,
+// //   queueLimit: 0
+// // });
 
 
 
-async function insertDetails(data) {
-  const sql ="INSERT INTO RDBMS set ?";
-  const [rows] = await connection.query(sql,data);
-  console.log("inserted");
-  return rows;
-}
+// // async function insertDetails(data) {
+// //   const sql ="INSERT INTO maniteja_RDBMS set ?";
+// //   const [rows] = await connection.query(sql,data);
+// //   console.log("inserted");
+// //   return rows;
+// // }
 
-app.post('/', async(req, res) => {
-    let data = req.body;
-    console.log(data);
-    // connection.query("INSERT INTO RDBMS set ?", data, function (err, result) {
-    //     if (err) throw err;
-    //     console.log("1 record inserted");
-    // });
-    const result = await insertDetails(data);
-    res.redirect("/");
-});
-
-
-async function getDetails(date) {
-  console.log(date+"get details");
-  const sql ="SELECT * FROM RDBMS WHERE Date=?";
-  const [rows] = await connection.query(sql,date);
-  console.log(rows);
-  return rows;
-}
+// // app.post('/', async(req, res) => {
+// //     let data = req.body;
+// //     console.log(data);
+// //     // connection.query("INSERT INTO RDBMS set ?", data, function (err, result) {
+// //     //     if (err) throw err;
+// //     //     console.log("1 record inserted");
+// //     // });
+// //     const result = await insertDetails(data);
+// //     res.redirect("/");
+// // });
 
 
-app.post('/events',express.json(),async(request, res) => {
-    console.log("request is done");
-    const {date}=request.body;
-    console.log(date);
-    const result = await getDetails(date);
-    res.send(result);
-    // connection.query("SELECT * FROM RDBMS WHERE Date=?",date, function (err, result, fields) {
-    //     if (err) throw err;;
-    //     res.send(result);
-    // });
-});
+// // async function getDetails(date) {
+// //   console.log(date+"get details");
+// //   const sql ="SELECT * FROM maniteja_RDBMS WHERE Date=?";
+// //   const [rows] = await connection.query(sql,date);
+// //   console.log(rows);
+// //   return rows;
+// // }
+
+
+
 
 app.listen(port,"0.0.0.0",() => {
     console.log(`Server is running on port ${port}`);
